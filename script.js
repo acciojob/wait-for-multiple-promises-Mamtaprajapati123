f8f0269e08f0ee2cac6294ffe5e82e04bbc3b54f
Mamtaprajapati123 <124058185+Mamtaprajapati123@users.noreply.github.com>
2023-09-03 20:37:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,49 +1,44 @@
-// Define the function to generate a random delay
+// script.js
+
 function randomDelay() {
-  return Math.floor(Math.random() * 2000) + 1000; // Returns a random number between 1000 and 3000 milliseconds (1-3 seconds)
+  return Math.floor(Math.random() * 2000) + 1000; // Random number between 1000 and 3000 milliseconds (1-3 seconds)
 }
 
-// Define a function to create a Promise with a random delay
 function createPromise(name) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     const delay = randomDelay();
     setTimeout(() => {
-      console.log(`${name} resolved after ${delay / 1000} seconds`);
-      resolve({ name, delay });
+      const success = Math.random() < 0.8; // Simulate 80% success rate
+      if (success) {
+        console.log(`${name} resolved after ${delay / 1000} seconds`);
+        resolve(delay);
+      } else {
+        console.error(`${name} failed`);
+        reject(`${name} failed`);
+      }
     }, delay);
   });
 }
 
-// Create an array of Promises
-const promises = [
+Promise.all([
   createPromise('Promise 1'),
   createPromise('Promise 2'),
   createPromise('Promise 3')
-];
-
-// Get the table body element
-const output = document.getElementById('output');
-
-// Add loading row
-const loadingRow = `<tr><td colspan="2">Loading...</td></tr>`;
-output.insertAdjacentHTML('beforeend', loadingRow);
-
-// Use Promise.all to wait for all Promises to resolve
-Promise.all(promises)
-  .then((results) => {
-    // Clear the loading row
+])
+  .then((values) => {
+    const output = document.getElementById('output');
     output.innerHTML = '';
 
-    // Populate the table with results
-    results.forEach((result, index) => {
-      const promiseName = result.name;
-      const timeTaken = (result.delay / 1000).toFixed(3);
+    values.forEach((value, index) => {
+      const promiseName = `Promise ${index + 1}`;
+      const timeTaken = `${(value / 1000).toFixed(3)}`;
       const row = `<tr><td>${promiseName}</td><td>${timeTaken}</td></tr>`;
       output.insertAdjacentHTML('beforeend', row);
     });
 
-    // Calculate and add the total time taken
-    const totalTimeTaken = (results.reduce((total, current) => total + current.delay, 0) / 1000).toFixed(3);
+    const totalTimeTaken = `${(
+      values.reduce((total, current) => total + current) / 1000
+    ).toFixed(3)}`;
     const totalRow = `<tr><td>Total</td><td>${totalTimeTaken}</td></tr>`;
     output.insertAdjacentHTML('beforeend', totalRow);
   })
